b6e989a2994b5541df6d74d7297ba4f681704233
lickhill <likhilnm17103@gmail.com>
2024-04-04 11:45:47 +0530

    made some changes

---
+++ cs50 WEB D/java script/index.js
@@ -0,0 +1,17 @@
+console.log("hello");
+
+let list={
+    name:"likhil",
+    age:19
+};
+
+list.name="maiya";
+list.age=29;
+
+list["name"]="maiyarse";
+list["age"]=90;
+
+let nam="name";
+list[nam]="lickhill";
+
+console.log(list);
